69dc622af460f40b2499074bfb6b7c4c7c34ddc7
Michele Simili <msimili@lx-msimili.qualcomm.com>
2019-04-30 14:21:42 +0100

    modified dotfile dir from home to effective directory

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,7 +6,7 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
+dir=${PWD}	                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim zshrc oh-my-zsh config/i3/config"    # list of files/folders to symlink in homedir .config missing
 
